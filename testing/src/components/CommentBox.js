8868b6f2acbb4219287aed62cece8994f8e19b9b
Pratik Ghadge <pratik.ghadge002@gmail.com>
2019-04-08 00:16:11 +0530

    Asserting Element Existence

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
 
-export default ()=>{
-    return <div>Comment Box</div>;
+// export default ()=>{
+//     //retrun <div>Box for Comment</div>;
+//     return <div>Box for Comment</div>;
+// }
+
+class CommentBox extends Component {
+  state = { comment: "" };
+
+  handlechange = event => {
+    this.setState({
+      comment: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    //Call an Action creator
+    //And save the comment
+    this.setState({ comment: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handlechange} value={this.state.comment} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
 }
+
+export default CommentBox;
--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,7 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App'
+//import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+import App from 'components/App'
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
-it('shows a comment box',()=>{
+// it('shows a comment box',()=>{
+//     // const div=document.create('div');
+//     const div=document.createElement('div');
+
+//     ReactDOM.render(<App/>,div);
+
+//     //Looks inside the div and check to see if the CommentBox is in there
+//     //console.log(div.innerHTML);
+//     //expect(div.innerHTML).toContain('Comment Box');
+//     //expect(div).toHaveAnInstanceOf(CommentBox);
+//     expect(div.innerHTML).toContain('Box for Comment');    
     
+//     ReactDOM.unmountComponentAtNode(div);
+// })
+
+// it('shows a comment box',()=>{
+//     const wrapped=shallow(<App/>);
+
+//     expect(wrapped.find(CommentBox).length).toEqual(1);
+// })
+
+// it('shows a comment list',()=>{
+//     const wrapped=shallow(<App/>);
+
+//     expect(wrapped.find(CommentList).length).toEqual(1);
+// })
+
+let wrapped;
+
+beforeEach(()=>{
+    wrapped=shallow(<App/>);
 })
+it('shows a comment box',()=>{
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+})
+
+it('shows a comment list',()=>{
+    expect(wrapped.find(CommentList).length).toEqual(1);
+})
+
