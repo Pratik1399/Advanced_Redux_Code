501a75924a0bfab1ad99e96efc84da2143a59eb4
Pratik1399 <pratik.ghadge20@gmail.com>
2020-05-20 18:06:05 +0530

    testing refactor

--- testing/README.md
+++ testing/README.md
@@ -1,7 +1,11 @@
 ## npx create-react-app testing
 
+## npm run start
+
 ## npm run test
 
 ## npm install --save redux react-redux
 
 ## npm install --save enzyme enzyme-adapter-react-16
+
+## npm install --save axios redux-promise moxios
--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,5 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
-it("shows a comment box", () => {});
+let wrapped;
+
+beforeEach(() => {
+	wrapped = shallow(<App />);
+});
+
+it("shows a comment box", () => {
+	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+	expect(wrapped.find(CommentList).length).toEqual(1);
+});
