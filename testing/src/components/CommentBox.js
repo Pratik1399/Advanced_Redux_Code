7d52763dddd64948ba4863573b24d8f828d37d30
Pratik1399 <pratik.ghadge20@gmail.com>
2021-02-21 16:46:16 +0530

    implemented the async.js middleware

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -3,34 +3,34 @@ import { connect } from "react-redux";
 import * as actions from "actions";
 
 class CommentBox extends Component {
-	state = { comment: "" };
+  state = { comment: "" };
 
-	handleChange = (event) => {
-		this.setState({ comment: event.target.value });
-	};
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		this.props.saveComment(this.state.comment);
-		this.setState({ comment: "" });
-	};
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.saveComment(this.state.comment);
+    this.setState({ comment: "" });
+  };
 
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<h4>Add a Comment</h4>
-					<textarea onChange={this.handleChange} value={this.state.comment} />
-					<div>
-						<button onSubmit={this.handleSubmit}>Submit Comment</button>
-					</div>
-				</form>
-				<button className="fetch-comments" onClick={this.props.fetchComments}>
-					Fetch Comments
-				</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button onSubmit={this.handleSubmit}>Submit Comment</button>
+          </div>
+        </form>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch Comments
+        </button>
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(CommentBox);
